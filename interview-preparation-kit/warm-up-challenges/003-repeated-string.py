665e61667eea5a7489b184e36630403d2eff7b60
Bryan Sanchez <sarabryanale@outlook.com>
2021-11-29 22:40:32 -0600

    feat: IPK-WUC-003-repeated-string

---
+++ interview-preparation-kit/warm-up-challenges/003-repeated-string.py
@@ -0,0 +1,26 @@
+# https://www.hackerrank.com/challenges/repeated-string/problem
+import math
+import re
+
+
+def find_a(string):
+    matches = re.findall(r"a", string)
+    return len(matches)
+
+
+def repeated_string(string, number):
+    # Factor to multiply string length to get number
+    factor = number // len(string)
+    # Get missing letters to get total 'number' value
+    module = number % len(string)
+
+    factor_matches = find_a(string) * factor
+    module_matches = find_a(string[0:module])
+    return factor_matches + module_matches
+
+
+if __name__ == "__main__":
+    repeated_string(
+        "kmretasscityylpdhuwjirnqimlkcgxubxmsxpypgzxtenweirknjtasxtvxemtwxuarabssvqdnktqadhyktagjxoanknhgilnm",
+        736778906400,
+    )
