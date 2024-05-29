800fcc88abc251f02d0ad2df5f1ab7dd2e38baaf
Bryan Sanchez <sarabryanale@outlook.com>
2024-05-28 20:51:30 -0600

    feat: subarray-division

---
+++ problem-solving/subarray-division.ts
@@ -0,0 +1,21 @@
+function birthday(chocolate: number[], day: number, length: number): number {
+  let segmentsCount = 0;
+
+  for (let i = 0; i < chocolate.length; i++) {
+    const segment = chocolate.slice(i, i + length);
+    const toBeShared =
+      segment.length === length &&
+      segment.reduce((item, acc) => item + acc, 0) === day;
+
+    if (toBeShared) {
+      segmentsCount++;
+    }
+  }
+
+  return segmentsCount;
+}
+
+
+console.log({ birthday: birthday([1, 2, 1, 3, 2], 3, 2) });
+console.log({ birthday: birthday([1, 1, 1, 1, 1], 3, 2) });
+console.log({ birthday: birthday([4], 4, 1) });
